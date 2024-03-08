9831f88d1429e960a4da2ece77d91a5df7796f80
cosmascodes <56020081+cosmascodes@users.noreply.github.com>
2024-03-08 14:15:01 +0300

    push

---
+++ maapeadventures/src/app/legal/page.js
@@ -0,0 +1,61 @@
+const Legal = () => {
+  return (
+    <section className=" mt-16 p-6 text-text">
+      <h1 className="text-center text-6xl font-duality ">Legal</h1>
+      <h2 className="text-2xl uppercase font-bold mt-7 mb-2">
+        section heading
+      </h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod
+        bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra
+        justo commodo. <br />
+        <br />
+        Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis
+        dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla
+        luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien
+        nunc eget odio.
+      </p>
+      <h2 className="text-2xl uppercase font-bold mt-7 mb-2">
+        section heading
+      </h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod
+        bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra
+        justo commodo. <br />
+        <br />
+        Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis
+        dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla
+        luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien
+        nunc eget odio.
+      </p>
+      <h2 className="text-2xl uppercase font-bold mt-7 mb-2">
+        section heading
+      </h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod
+        bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra
+        justo commodo. <br />
+        <br />
+        Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis
+        dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla
+        luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien
+        nunc eget odio.
+      </p>
+      <h2 className="text-2xl uppercase font-bold mt-7 mb-2">
+        section heading
+      </h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod
+        bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra
+        justo commodo. <br />
+        <br />
+        Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis
+        dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla
+        luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien
+        nunc eget odio.
+      </p>
+    </section>
+  );
+};
+
+export default Legal;
