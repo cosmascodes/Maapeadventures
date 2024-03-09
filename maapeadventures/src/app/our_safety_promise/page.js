af5a9308018e3c45e6d6cdf3ea3591e1dad8ea31
cosmascodes <56020081+cosmascodes@users.noreply.github.com>
2024-03-09 16:21:13 +0300

    upload

---
+++ maapeadventures/src/app/our_safety_promise/page.js
@@ -0,0 +1,44 @@
+const Safety = () => {
+  return (
+    <section className=" mt-16 p-6 text-text">
+      <h1 className="text-center text-6xl font-duality mb-6">
+        Our Safety Promise
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod
+        bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra
+        justo commodo.
+        <br />
+        <br />
+        Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis
+        dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla
+        luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien
+        nunc eget odio.
+        <br />
+        <br />
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod
+        bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra
+        justo commodo.
+        <br />
+        <br />
+        Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis
+        dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla
+        luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien
+        nunc eget odio.
+        <br />
+        <br />
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod
+        bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra
+        justo commodo.
+        <br />
+        <br />
+        Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis
+        dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla
+        luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien
+        nunc
+      </p>
+    </section>
+  );
+};
+
+export default Safety;
