861e9115fdda1c3abee10d55ec54fcfa0437bfe3
cosmascodes <56020081+cosmascodes@users.noreply.github.com>
2024-03-24 21:23:03 +0300

    push

--- maapeadventures/src/components/story.js
+++ maapeadventures/src/components/story.js
@@ -1,11 +1,11 @@
 const AdventureStory = (props) => {
   return (
-    <div className="flex flex-col md:flex-row justify-center items-center mt-8 ml-6 md:ml-0">
+    <div className="flex flex-col md:flex-row justify-center w-60 md:w-fit items-center mt-8  ml-12 md:ml-0">
       <div className=" w-full md:w-38 mb-4 md:mb-0">
         <img src={"/images/home_page/u30_a.svg"} className="w-full" />
       </div>
       <div className="font-mono uppercase md:pl-4 ">
-        <h2 className="text-sm md:text-2xl mb-1 font-semibold text-primary w-40">
+        <h2 className="text-2xl mb-1 font-semibold text-primary w-40">
           {props.title}
         </h2>
         <p className="text-sm text-tertiary">{props.name}</p>
