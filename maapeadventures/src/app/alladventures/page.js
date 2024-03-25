6167da7a167dea6913a2ea6e88ab8799d5d010dc
cosmascodes <56020081+cosmascodes@users.noreply.github.com>
2024-03-25 15:40:42 +0300

    push

--- maapeadventures/src/app/alladventures/page.js
+++ maapeadventures/src/app/alladventures/page.js
@@ -6,7 +6,7 @@ const AllAdventures = () => {
   return (
     <section className="md:bg-hero-pattern bg-[length:22vw] bg-[center_right_24rem] bg-no-repeat flex flex-col justify-center items-center pt-24 md:pb-12 py-8">
       <h1 className="font-duality text-6xl text-text mb-8 md:mb-12 text-center">
-        Choose an adventure
+        Choose an Adventure
       </h1>
       <div className="w-full md:w-fit pt-14 pb-20 md:px-20 md:rounded-[60px] bg-primary">
         <h1 className="md:text-center mb-5 text-2xl font-mono font-bold text-white ml-12 md:ml-0">
